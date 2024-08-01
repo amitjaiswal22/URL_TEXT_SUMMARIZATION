920c3172fdd22cde85a76bc0bed03a82eaa69acb
amitjaiswal22 <114880035+amitjaiswal22@users.noreply.github.com>
2024-08-01 11:12:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.subheader('Summarize URL')
 
 ## Get the Groq API Key and url(YT or website)to be summarized
 with st.sidebar:
-    groq_api_key=st.text_input("Groq API Key",value="",type="password")
+    groq_api_key=st.text_input("Groq API Key",value="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",type="password")
 
 generic_url=st.text_input("URL",label_visibility="collapsed")
 
